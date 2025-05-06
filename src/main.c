ab7db4ab9ecee47a5e6b303aeec59a4e916b69ad
PTRRR <pietro.alberti@bluewin.ch>
2025-05-06 13:08:23 +0200

    Add base program

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // Exemple pour le type char
+    printf("Taille de char : %zu octets\n", sizeof(char));
+
+    // Ajoutez le code pour les autres types de données ici
+
+    return 0;
+}
